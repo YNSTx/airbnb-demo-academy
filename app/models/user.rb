12ebc52d570e54e45e2a202fc5191c463f1ce9a8
mezmer aboubaker <bakermezmer@gmail.com>
2024-01-23 12:27:45 +0100

    seed done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_many :sessions, trough: :reservations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :first_name, presence: true
+         validates :last_name, presence: true, uniqueness: true
 end
